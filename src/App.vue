5e042ab57e579107c6a8be8b0f80eff4e7d8a82c
irigyano <110440243+irigyano@users.noreply.github.com>
2024-03-15 12:54:01 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+  <!-- <nav>
+    <RouterLink to="/">Home</RouterLink>
+  </nav> -->
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Placeholder from '../components/Placeholder.vue'
+</script>
+
+<template>
+  <Placeholder msg="Placeholder" />
+</template>
