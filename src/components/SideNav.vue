31289e1742140ed7956c403e31e00a7f2c90a931
irigyano <110440243+irigyano@users.noreply.github.com>
2024-03-18 20:45:01 +0800

    feat: routing

---
+++ src/components/SideNav.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="flex-col flex-[0.2] hidden sm:flex">
+    <RouterLink to="/">首頁</RouterLink>
+    <RouterLink to="/sense">個人資料</RouterLink>
+  </nav>
+</template>
---
+++ src/views/UserView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>{{ $route.params.user }}</div>
+</template>
