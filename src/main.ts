0d4942e74b0c131e744f6d3b22936247fb06e3d0
irigyano <110440243+irigyano@users.noreply.github.com>
2024-04-14 17:39:22 +0800

    chore: add pinia

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import { createPinia } from 'pinia'
 
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
 app.use(VueQueryPlugin)
 
