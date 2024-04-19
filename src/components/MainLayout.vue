7f2d576daacab9f7b57139ea011cef7ee97060f6
irigyano <110440243+irigyano@users.noreply.github.com>
2024-04-19 22:03:44 +0800

    feat: routes key

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import SideNav from '@/components/SideNav.vue'
 import SideFeed from '@/components/SideFeed.vue'
+const route = useRoute()
 </script>
 
 <template>
   <SideNav />
   <main class="flex flex-1 max-w-[600px] border-x-[1px] border-border">
-    <RouterView />
+    <!-- Potential performance issue -->
+    <!-- https://stackoverflow.com/questions/69638667/vue-router-is-changing-the-url-but-not-re-rendering-the-component -->
+    <RouterView :key="route.path" />
   </main>
   <SideFeed />
 </template>
