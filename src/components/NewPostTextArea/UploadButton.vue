bb9b9dde230ca5710fbb495dbc8a404b6019abf6
irigyano <110440243+irigyano@users.noreply.github.com>
2024-04-22 16:14:51 +0800

    feat: replace base64 with storage

--- src/components/NewPostTextArea/UploadButton.vue
+++ src/components/NewPostTextArea/UploadButton.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { ImageUp } from 'lucide-vue-next'
+import { useUploadImage } from '@/hooks/useUploadImage'
+
 const uploadImageButton = ref<HTMLInputElement | null>(null)
 const imageBase64 = defineModel<string>('imageBase64')
 
-function sumbitPost(e: Event) {
-  if (!e.target) return
-  const imageFileList = (e.target as HTMLInputElement).files
-  if (!imageFileList) return
-  const inputImage = imageFileList[0]
-  if (typeof inputImage === 'undefined') return
+const isUploading = ref(false)
 
-  const fileReader = new FileReader()
-  fileReader.onloadend = function () {
-    const baseString = fileReader.result as string
-    imageBase64.value = baseString
-  }
-  fileReader.readAsDataURL(inputImage)
+async function uploadPostImage(event: Event) {
+  if (isUploading.value) return
+  isUploading.value = true
+
+  try {
+    const url = await useUploadImage(event, 'post')
+    imageBase64.value = url
+  } catch (e) {}
+  setTimeout(() => {
+    isUploading.value = false
+  }, 2000)
 }
 </script>
 
@@ -25,6 +27,12 @@ function sumbitPost(e: Event) {
     <div class="hover:bg-blue-500 bg-opacity-20 duration-200 rounded-full p-2">
       <ImageUp :size="20" />
     </div>
-    <input ref="uploadImageButton" class="hidden" type="file" @change="sumbitPost" />
+    <input
+      ref="uploadImageButton"
+      class="hidden"
+      type="file"
+      @change="uploadPostImage"
+      accept="image/*"
+    />
   </div>
 </template>
