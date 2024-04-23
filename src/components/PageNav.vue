c27621a9ecdca7e75f316f296327d12a7d05f231
irigyano <110440243+irigyano@users.noreply.github.com>
2024-04-23 22:18:40 +0800

    feat: pagenav programmatic navigation

--- src/components/PageNav.vue
+++ src/components/PageNav.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import { useRouter } from 'vue-router'
 import { ChevronLeft } from 'lucide-vue-next'
 const { title } = defineProps<{ title: string }>()
+const router = useRouter()
 </script>
 
 <template>
   <nav
     class="flex fixed top-0 w-full sm:sticky z-10 backdrop-blur-md items-center bg-background/50"
   >
-    <RouterLink to="/" class="p-4 hover:text-primary duration-300">
+    <a @click="router.go(-1)" class="p-4 hover:text-primary duration-300">
       <ChevronLeft :size="24" />
-    </RouterLink>
+    </a>
     <div>
       <div class="font-bold text-lg">{{ title }}</div>
       <slot></slot>
