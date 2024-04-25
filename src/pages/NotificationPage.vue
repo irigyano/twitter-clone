51c9f1611ca5f57ba65d98f14d6cc030a6b48744
irigyano <110440243+irigyano@users.noreply.github.com>
2024-04-25 15:39:55 +0800

    feat: post dropdown menu & notify page nav

--- src/pages/NotificationPage.vue
+++ src/pages/NotificationPage.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PageNav from '@/components/PageNav.vue'
+</script>
 
 <template>
-  <div>no notifications yet</div>
+  <div class="flex-1">
+    <PageNav :title="'通知'" />
+
+    <div class="text-center font-bold text-3xl">還沒有可查看的資訊</div>
+  </div>
 </template>
