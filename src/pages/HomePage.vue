0ef63e42d7b66656a0409c30c286561976b27729
irigyano <110440243+irigyano@users.noreply.github.com>
2024-04-27 00:28:19 +0800

    refactor: delete post feed component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
 import NewPostTextArea from '@/components/NewPostTextArea/NewPostTextArea.vue'
-import PostFeed from '@/components/Post/PostFeed.vue'
 import Category from '@/components/Layout/Category.vue'
+import { useQuery } from '@tanstack/vue-query'
+import Post from '@/components/Post/Post.vue'
+import Loading from '@/components/Loading.vue'
+import { getPosts } from '@/utils/query'
+
+const {
+  isLoading,
+  isError,
+  data: posts,
+  error
+} = useQuery({
+  queryKey: ['posts'],
+  queryFn: getPosts,
+  gcTime: 0
+})
 </script>
 
 <template>
   <div class="flex flex-col items-center w-full">
     <Category />
     <NewPostTextArea />
-    <PostFeed />
+    <div v-if="isLoading" class="fixed top-[50%]">
+      <Loading />
+    </div>
+    <div v-else-if="posts?.length" class="w-full flex flex-col">
+      <Post v-for="post in posts" :author="post.author" :post="post.post" :key="post.post.id" />
+    </div>
+    <div v-else-if="isError">Error: {{ error }}</div>
+    <div v-else>Add a new post!</div>
   </div>
 </template>
