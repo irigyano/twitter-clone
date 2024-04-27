cd1609905b9fc211e4ef4451eac645f3230bfdd9
irigyano <110440243+irigyano@users.noreply.github.com>
2024-04-27 14:14:15 +0800

    chore: env

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/types/supabase'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_ENDPOINT
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_TOKEN
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
