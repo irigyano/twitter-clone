0b1d32dfa5ae04546b8ea2d8055e66eeee8f00a8
irigyano <110440243+irigyano@users.noreply.github.com>
2024-04-29 04:26:42 +0800

    feat: retweet button dropdown

--- src/components/Post/RetweetButton.vue
+++ src/components/Post/RetweetButton.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 import { supabase } from '@/utils/supabase'
 import { useUserStore } from '@/stores/user'
-import { Repeat2 } from 'lucide-vue-next'
+import { Repeat2, PencilLine } from 'lucide-vue-next'
 import type { PostInfo } from '@/types/queries'
 import { computed } from 'vue'
 import { useQueryClient } from '@tanstack/vue-query'
@@ -34,16 +40,30 @@ async function retweet() {
 </script>
 
 <template>
-  <button
-    @click.stop="retweet"
-    class="flex hover:text-green-500 group gap-1 items-center duration-300"
-    :class="isRetweeted && 'text-green-500'"
-  >
-    <div
-      class="group-hover:bg-green-500 group-hover:text-green-500 group-hover:bg-opacity-30 rounded-full p-2 cursor-pointer duration-300"
-    >
-      <Repeat2 :size="18" />
-    </div>
-    {{ retweets }}
-  </button>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child @click.stop>
+      <button
+        class="flex hover:text-green-500 group gap-1 items-center duration-300"
+        :class="isRetweeted && 'text-green-500'"
+      >
+        <div
+          class="group-hover:bg-green-500 group-hover:text-green-500 group-hover:bg-opacity-30 rounded-full p-2 cursor-pointer duration-300"
+        >
+          <Repeat2 :size="18" />
+        </div>
+        {{ retweets }}
+      </button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent class="min-w-0">
+      <DropdownMenuItem @click.stop="retweet" class="cursor-pointer flex gap-2 text-base">
+        <Repeat2 />
+        <span v-if="!isRetweeted">轉發</span>
+        <span v-else-if="isRetweeted">取消轉發</span>
+      </DropdownMenuItem>
+      <DropdownMenuItem disabled class="flex gap-2 text-base">
+        <PencilLine />
+        <span>引用</span>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
